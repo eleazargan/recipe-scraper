6328b65067275eb71902c285995efe9a85a1d93a
eleazargan <eleazar11egkh@gmail.com>
2020-02-22 15:29:40 +0800

    Fix cors

--- web/src/components/Search.js
+++ web/src/components/Search.js
@@ -19,7 +19,7 @@ class Search extends React.Component {
     }
 
     _onUpdateMenu(event) {
-        this.setState({menu: event.target.value});
+        this.setState({ menu: event.target.value });
     }
 
     async _searchMenu() {
@@ -28,10 +28,13 @@ class Search extends React.Component {
             loading: true
         });
 
-        const url = 'http://localhost:5000/search?search_term=' + this.state.menu;
+        const url = '//localhost:5000/search?search_term=' + this.state.menu;
 
         try {
-            const response = await fetch(url);
+            const response = await fetch(url, {
+                method: 'get',
+                headers: { 'Access-Control-Allow-Origin': '*' },
+            });
             const json = await response.json();
             this.setState({
                 servings: json.serving,
@@ -48,8 +51,8 @@ class Search extends React.Component {
     }
 
     convertUnicode(input) {
-        return input.replace(/\\u(\w\w\w\w)/g,function(a,b) {
-            var charcode = parseInt(b,16);
+        return input.replace(/\\u(\w\w\w\w)/g, function (a, b) {
+            var charcode = parseInt(b, 16);
             return String.fromCharCode(charcode);
         });
     }
@@ -58,49 +61,49 @@ class Search extends React.Component {
         return (
             <div className="max-w-md overflow-hidden shadow-md m-auto">
                 <nav className="flex items-center justify-between flex-wrap bg-purple-500 p-6 text-center">
-                <div className="w-full block flex-grow lg:w-auto">
-                    <div className="text-sm lg:flex-grow m-auto">
-                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-auto py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-w-hite focus:border-purple-500"
-                        type="text" placeholder="Search for recipes" value={this.state.menu}
-                        onChange={this._onUpdateMenu}/>
-                    <button className="shadow bg-purple-900 hover:bg-purple-800 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                        type="button" onClick={this._searchMenu}>
-                        Search Recipe
+                    <div className="w-full block flex-grow lg:w-auto">
+                        <div className="text-sm lg:flex-grow m-auto">
+                            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-auto py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-w-hite focus:border-purple-500"
+                                type="text" placeholder="Search for recipes" value={this.state.menu}
+                                onChange={this._onUpdateMenu} />
+                            <button className="shadow bg-purple-900 hover:bg-purple-800 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                                type="button" onClick={this._searchMenu}>
+                                Search Recipe
                     </button>
+                        </div>
                     </div>
-                </div>
                 </nav>
                 {this.state.loading ? <div>
                     Searching for your food!
                 </div> :
-                <div>
-                    <img class="w-full" src={this.state.coverImage} alt={this.state.menu}/>
-                    <div className="px-6 py-4">
-                        <div className="font-bold text-xl mb-2">{this.state.search && (this.state.menu)}</div>
-                        {this.state.search && (<p className="mb-4 font-bold">Servings: {this.state.servings} person</p>)}
-                        {(this.state.search && this.state.ingredients) && (
-                            <div className="mb-8 px-4">
-                                <div className="mb-2 text-xl text-bold">Ingredients</div>
-                                <ul className="list-disc">
-                                    {this.state.ingredients.map((item, index) => (
-                                        <li>{this.convertUnicode(item)}</li>
-                                    ))}
-                                </ul>
-                            </div>
-                        )}
-                        {(this.state.search && this.state.directions) && (
-                            <div className="px-4">
-                                <div className="mb-2 text-xl text-bold">Cooking Directions</div>
-                                <ol className="list-decimal">
-                                    {this.state.directions.map((item, index) => (
-                                        <li className="mb-4">{this.convertUnicode(item)}</li>
-                                    ))}
-                                </ol>
-                            </div>
-                        )}
-                    </div>
-                </div>}
-            </div>  
+                    <div>
+                        <img className="w-full" src={this.state.coverImage} alt={this.state.menu} />
+                        <div className="px-6 py-4">
+                            <div className="font-bold text-xl mb-2">{this.state.search && (this.state.menu)}</div>
+                            {this.state.search && (<p className="mb-4 font-bold">Servings: {this.state.servings} person</p>)}
+                            {(this.state.search && this.state.ingredients) && (
+                                <div className="mb-8 px-4">
+                                    <div className="mb-2 text-xl text-bold">Ingredients</div>
+                                    <ul className="list-disc">
+                                        {this.state.ingredients.map((item, index) => (
+                                            <li>{this.convertUnicode(item)}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                            {(this.state.search && this.state.directions) && (
+                                <div className="px-4">
+                                    <div className="mb-2 text-xl text-bold">Cooking Directions</div>
+                                    <ol className="list-decimal">
+                                        {this.state.directions.map((item, index) => (
+                                            <li className="mb-4">{this.convertUnicode(item)}</li>
+                                        ))}
+                                    </ol>
+                                </div>
+                            )}
+                        </div>
+                    </div>}
+            </div>
         );
     }
 }
